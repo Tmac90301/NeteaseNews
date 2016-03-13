f374bf001880f5fff64201f09f271a01f8f159a7
MyMacbookPro <Henry_P@mymacbookprodemacbook-pro.local>
2016-03-13 19:58:34 +0800

    初始化项目

---
+++ 网易新闻/网易新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易新闻
+//
+//  Created by MyMacbookPro on 16/3/13.
+//  Copyright © 2016年 MyMacbookPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
